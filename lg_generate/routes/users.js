52849a5e6acc273ae90577010254e3de8892be42
Chirag <chiragthakar066@gmail.com>
2024-05-29 08:44:47 +0530

    first commit

--- lg_generate/routes/users.js
+++ lg_generate/routes/users.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/registration', function(req, res, next) {
-  console.log("req.data");
-  let z =(JSON.parse(req.body));
-  console.log(z);
+  // console.log(req.data);
+  console.log(req.body.regFormData);
+  // console.log(req.body.data);
+  // let z =JSON.parse(req.body);
+  // console.log(z);
   res.send("abc");
 });
 
